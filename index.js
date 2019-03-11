6801f53ec7be58a0921c71466b557e7c2618a265
webdev-gmg <41268601+webdev-gmg@users.noreply.github.com>
2019-03-11 17:44:58 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 
-var statsDate="2019-03-04"
+var statsDate="2019-03-05"
 var caloriesLeft = ""
 var goalsCaloriesOut=""
 var activityCalorie = ""
 
+setInterval(activitySteps(), 3000);
 setTimeout(runStats(statsDate),3000)
 //$('#runStats').on('click',runStats)
 $('#searchdate').on('change',function(){
@@ -12,7 +13,7 @@ $('#searchdate').on('change',function(){
 })
 
 function runStats(statsDate){
-    if(statsDate != "2019-03-04")
+    if(statsDate != "2019-03-05")
     {
     statsDate = $('#searchdate').val();
     }
@@ -22,7 +23,7 @@ var settings = {
     "method": "GET",
     "headers": {
       "Content-Type": "application/json",
-      "Authorization": "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyMkRLR1ciLCJzdWIiOiI1V1RYUTciLCJpc3MiOiJGaXRiaXQiLCJ0eXAiOiJhY2Nlc3NfdG9rZW4iLCJzY29wZXMiOiJyc29jIHJzZXQgcmFjdCBybG9jIHJ3ZWkgcmhyIHJwcm8gcm51dCByc2xlIiwiZXhwIjoxNTUxOTMzNTU1LCJpYXQiOjE1NTE5MDQ3NTV9.QqsYFxmfvd7SMRMHZ6oUqwPMJyCWlyffh4nA9A8SgjA",
+      "Authorization": "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyMkRLR1ciLCJzdWIiOiI1V1RYUTciLCJpc3MiOiJGaXRiaXQiLCJ0eXAiOiJhY2Nlc3NfdG9rZW4iLCJzY29wZXMiOiJyc29jIHJhY3QgcnNldCBybG9jIHJ3ZWkgcmhyIHJudXQgcnBybyByc2xlIiwiZXhwIjoxNTUxOTk5OTEwLCJpYXQiOjE1NTE5NzExMTB9.fuBeqySeb5NwK8tzCSYuEAyqjTDJu2FfihEpizijfEI",
     }
 
   }
@@ -59,7 +60,7 @@ var settings = {
     "method": "GET",
     "headers": {
       "Content-Type": "application/json",
-      "Authorization": "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyMkRLR1ciLCJzdWIiOiI1V1RYUTciLCJpc3MiOiJGaXRiaXQiLCJ0eXAiOiJhY2Nlc3NfdG9rZW4iLCJzY29wZXMiOiJyc29jIHJzZXQgcmFjdCBybG9jIHJ3ZWkgcmhyIHJwcm8gcm51dCByc2xlIiwiZXhwIjoxNTUxOTMzNTU1LCJpYXQiOjE1NTE5MDQ3NTV9.QqsYFxmfvd7SMRMHZ6oUqwPMJyCWlyffh4nA9A8SgjA",
+      "Authorization": "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyMkRLR1ciLCJzdWIiOiI1V1RYUTciLCJpc3MiOiJGaXRiaXQiLCJ0eXAiOiJhY2Nlc3NfdG9rZW4iLCJzY29wZXMiOiJyc29jIHJhY3QgcnNldCBybG9jIHJ3ZWkgcmhyIHJudXQgcnBybyByc2xlIiwiZXhwIjoxNTUxOTk5OTEwLCJpYXQiOjE1NTE5NzExMTB9.fuBeqySeb5NwK8tzCSYuEAyqjTDJu2FfihEpizijfEI",
     }
   }
  
@@ -71,4 +72,88 @@ var settings = {
     $('#remainingCalories').text(caloriesLeft)
     console.log(caloriesLeft)
   });}
+
+
+
+  var url = `https://api.fitbit.com/1/user/-/foods/log/date/${statsDate}.json`;
+  axios.get(url)
+  .then(data =>console.log(data))
+  .catch(err => console.log())
+
+
+  
+
+function activitySteps(){
+    var label =[];
+var dateTime = [];
+  var settings = {
+    "url": "https://api.fitbit.com/1/user/-/activities/steps/date/today/1m.json",
+    "method": "GET",
+    "headers": {
+      "Content-Type": "application/json",
+      "Authorization": "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyMkRLR1ciLCJzdWIiOiI1V1RYUTciLCJpc3MiOiJGaXRiaXQiLCJ0eXAiOiJhY2Nlc3NfdG9rZW4iLCJzY29wZXMiOiJyc29jIHJzZXQgcmFjdCBybG9jIHJ3ZWkgcmhyIHJwcm8gcm51dCByc2xlIiwiZXhwIjoxNTUyMzU5MzYzLCJpYXQiOjE1NTIzMzA1NjN9.C3RIeuKjsMHe8v6ssanQ5OAczb7rn8xOqq3qmMgm08c"
+    }
+  }
+ 
+  $.ajax(settings).done(function (response) {
+    
+  //console.log(response[0][1][0].dateTime)
+ const entries = Object.entries(response)
+// console.log(entries[0][1])
+ var a = entries[0][1]
+
+ for(i=0;i<a.length;i++)
+ {
+   label.push(parseInt(a[i].value)) 
+   dateTime.push(a[i].dateTime)
+ //  console.log(a[i].value)  
+ //console.log(label)
+ }
+ console.log(label)
+ console.log(dateTime)
+ var ctx = document.getElementById('myChart').getContext('2d');
+var chart = new Chart(ctx, {
+    // The type of chart we want to create
+    type: 'bar',
+
+    // The data for our dataset
+    data: {
+        labels: dateTime,
+        datasets: [{
+           label:"Steps",
+           backgroundColor : getRandomColor(),
+            // backgroundColor:["rgba(255, 99, 132, 0.2)","rgba(255, 159, 64, 0.2)","rgba(255, 205, 86, 0.2)","rgba(75, 192, 192, 0.2)","rgba(54, 162, 235, 0.2)","rgba(153, 102, 255, 0.2)","rgba(201, 203, 207, 0.2)"],
+            borderColor:getRandomColor(),
+            data: label,
+            beginAtZero:true,
+            borderWidth: 1
+        }]
+    },
+
+    // Configuration options go here
+    options: {responsive: false,scales: {
+        yAxes: [{
+            ticks: {
+                beginAtZero:true
+            }
+        }]
+    }}
+});
+  });
+
+}
+
+setInterval(activitySteps, 7000);
+
+
+function getRandomColor() {
+    var letters = '0123456789ABCDEF'.split('');
+    var color = '#';
+    for (var i = 0; i < 6; i++ ) {
+        color += letters[Math.floor(Math.random() * 6)];
+    }
+    return color;
+}
+
+
 
